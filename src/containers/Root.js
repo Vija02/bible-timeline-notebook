c2a3df365bd2a2186817ff37b73bf63811db55d8
Michael Salim <salim_michaelmi@yahoo.com>
2018-01-16 19:58:52 +0000

    feat: add graphql

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Home from './Home'
 
 class Root extends Component {
+  createClient() {
+    const cache = new InMemoryCache({
+      dataIdFromObject: o => o.nodeId,
+    });
+    const httpLink = createHttpLink({
+      uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+    })
+    return new ApolloClient({
+      cache: cache,
+      link: httpLink,
+      defaultOptions: { query: { notifyOnNetworkStatusChange: true } }
+    });
+  }
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route path="/" component={Home} />
-        </Switch>
-      </Router>
+      <ApolloProvider client={this.createClient()}>
+        <Router>
+          <Switch>
+            <Route path="/" component={Home} />
+          </Switch>
+        </Router>
+      </ApolloProvider>
     );
   }
 }
