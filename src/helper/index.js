f40b21e9a87378d9bd2abf7cf547278fb35507f7
Michael Salim <salim_michaelmi@yahoo.com>
2018-01-18 23:32:25 +0000

    feat: add unformatBook

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,3 +1,11 @@
+export const toTitleCase = (str) => {
+  return str.replace(/\w\S*/g, (txt) => { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
+}
+
 export const formatBook = (book) => {
   return book.replace(/ /g, '_').toLowerCase();
+}
+
+export const unformatBook = (book) => {
+  return toTitleCase(book.replace(/_/g, ' '));
 }
