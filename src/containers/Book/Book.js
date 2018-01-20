47acfe455710b8d33c5a86e902db670a68b34daa
Michael Salim <salim_michaelmi@yahoo.com>
2018-01-20 00:02:44 +0000

    feat: update getting data from db

--- src/containers/Book/Book.js
+++ src/containers/Book/Book.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { compose, graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 
-import { unformatBook } from 'helper'
+import { bookIdFromName, unformatBook } from 'helper'
 
 class Book extends Component {
   render() {
@@ -11,15 +11,15 @@ class Book extends Component {
     return (
       <div style={{ paddingLeft: 40 }}>
         <p>{unformatBook(this.props.match.params.bookName)}</p>
-        <p style={{ width: "30vw" }}>{loading || error ? "-" : this.props.data.bookSummaries.nodes[0].summary}</p>
+        <p style={{ width: "30vw" }}>{loading || error ? "-" : this.props.data.bookSummaries.nodes.length > 0 ? this.props.data.bookSummaries.nodes[0].summary : "No summary"}</p>
       </div>
     );
   }
 }
 
 export default compose(graphql(gql`
-mutation($bookName: String!, $summary: String!){
-  createBookSummary(input: {bookSummary:{bookName: $bookName, summary: $summary}}){
+mutation($bookId: Int!, $summary: String!){
+  createBookSummary(input: {bookSummary:{bookId: $bookId, summary: $summary}}){
     bookSummary{
       id
     }
@@ -27,14 +27,14 @@ mutation($bookName: String!, $summary: String!){
 }
 `, {
     props: ({ mutate }) => ({
-      createBookSummary: ({ bookName, summary }) => mutate({ variables: { bookName, summary } })
+      createBookSummary: ({ bookId, summary }) => mutate({ variables: { bookId, summary } })
     })
   }), graphql(gql`
-query($bookName: String!){
-  bookSummaries: allBookSummaries(condition:{bookName: $bookName}){
+query($bookId: Int!){
+  bookSummaries: allBookSummaries(condition:{bookId: $bookId}){
     nodes{
       summary
     }
   }
 }
-`, { options: ({ match }) => ({ variables: { bookName: match.params.bookName } }) }))(Book)
+`, { options: ({ match }) => ({ variables: { bookId: bookIdFromName(match.params.bookName) } }) }))(Book)
