7ab26fdb049b84901b0f881a56a8b4a8db659280
Michael Salim <salim_michaelmi@yahoo.com>
2018-01-25 14:44:00 +0000

    feat: resize on selected

--- src/containers/Home/BookContainer.js
+++ src/containers/Home/BookContainer.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
+import sizeMe from 'react-sizeme'
+import { Motion, spring } from 'react-motion'
 
 import { formatBook } from 'helper'
 
 import styles from './BookContainer.module.css'
 
-export default class BookContainer extends Component {
+class BookContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { width: 0 };
+  }
+  componentWillUpdate(nextProps, nextState) {
+    // Get inital size
+    const selected = nextProps.match && nextProps.match.params.bookName === formatBook(nextProps.book.bookName)
+    if (selected && this.state.width === 0) {
+      this.setState({ width: nextProps.size.width })
+    }
+  }
   render() {
+    const selected = this.props.match && this.props.match.params.bookName === formatBook(this.props.book.bookName)
+
+    const motionStyle = selected ? {
+      width: spring(90) // vw
+    } : {
+        width: spring(this.state.width / document.documentElement.clientWidth * 100)
+      }
+
     return (
       <div className={styles.bookContainer}>
-        <div className={styles.bookSelector} onMouseDown={(e) => { e.stopPropagation() }} onClick={() => {
-          this.props.history.push(`/book/${formatBook(this.props.book.bookName)}`)
-        }}>
-          {this.props.book.bookName}
-        </div>
+        <Motion style={motionStyle}>
+          {({ width }) => {
+            return (
+              <div className={selected ? styles.selectedBookSelector : styles.bookSelector} onMouseDown={(e) => { e.stopPropagation() }} style={width !== 0 && this.props.size.width ? { width: `${width}vw` } : {}} onClick={() => {
+                this.props.history.push(`/book/${formatBook(this.props.book.bookName)}`)
+              }}>
+                {this.props.book.bookName}
+              </div>
+            )
+          }}
+        </Motion>
       </div>
     );
   }
 }
+
+export default sizeMe()(BookContainer)
