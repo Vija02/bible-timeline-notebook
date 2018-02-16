64a43a1e755210969c51e059ebd24bd1d5d9a3d6
Michael Salim <salim_michaelmi@yahoo.com>
2018-02-16 21:22:11 +0000

    feat: make home will grids

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,18 +2,24 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
 
 import Book from 'containers/Book'
+import Sidebar from 'containers/Sidebar'
 
 import MainTimeline from './MainTimeline'
+
+import styles from './Home.module.css'
 
 export default class HomeIndex extends Component {
   render() {
     return (
-      <React.Fragment>
-        <MainTimeline {...this.props} />
-        <Switch>
-          <Route path="/book" component={Book} />
-        </Switch>
-      </React.Fragment>
+      <div className={styles.container}>
+        <Sidebar className={styles.sidebar} />
+        <MainTimeline className={styles.main} />
+        <div className={styles.bottom}>
+          <Switch>
+            <Route path="/book" component={Book} />
+          </Switch>
+        </div>
+      </div>
     );
   }
 }
