ccc98bba2cc661f5a7d4310fb96524b790fc8185
Michael Salim <salim_michaelmi@yahoo.com>
2018-02-16 20:21:38 +0000

    feat: add summary

--- src/containers/Sidebar/AddSummary/index.js
+++ src/containers/Sidebar/AddSummary/index.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import linkState from 'linkstate'
+
+import BibleVerseSelector from 'components/BibleVerseSelector'
 
 export default class AddSummaryIndex extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      verse1: null, verse2: null,
+      title: "", summary: ""
+    };
+  }
   render() {
     return (
-      <div>
-        Add Summary
+      <div style={{ display: "flex", flexDirection: "column", marginBottom: 50 }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <a>From</a>
+          <BibleVerseSelector onChange={linkState(this, "verse1")} />
+          <a>To</a>
+          <BibleVerseSelector onChange={linkState(this, "verse2")} />
+        </div>
+        <p>Title</p>
+        <input type="text" value={this.state.title} onChange={linkState(this, "title")} />
+        <p>Summary</p>
+        <textarea style={{ height: "100px" }} value={this.state.summary} onChange={linkState(this, "summary")} />
+        <button style={{ width: "50px" }} disabled={!this.state.verse1 || !this.state.verse2}>Add</button>
       </div>
     );
   }
--- src/containers/Sidebar/index.js
+++ src/containers/Sidebar/index.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 
 import DropdownMenu from 'components/DropdownMenu'
 
+import AddSummary from './AddSummary'
+
 import styles from './Sidebar.module.css'
 
 export default class SideBarIndex extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { opened: false };
+  }
   render() {
     return (
       <div className={`${this.props.className} ${styles.container}`}>
@@ -14,7 +20,10 @@ export default class SideBarIndex extends Component {
         <DropdownMenu
           iconClassName="fas fa-plus"
           title="Add Summary"
+          onClick={() => { this.setState({ opened: !this.state.opened }) }}
+          opened={this.state.opened}
         >
+          <AddSummary />
         </DropdownMenu>
       </div>
     );
