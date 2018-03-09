180466e9fb22f08749f9e68a5e46658c398f6dad
Michael Salim <salim_michaelmi@yahoo.com>
2018-03-09 19:06:38 +0000

    feat: add dotted lines to show where to clarify

--- src/containers/Home/SummaryContainer/index.js
+++ src/containers/Home/SummaryContainer/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 
 import styles from './SummaryContainer.module.css'
 
+const borderSideHeight = 20
+
 export default class SummaryContainer extends Component {
 	render() {
 		return (
@@ -9,18 +11,36 @@ export default class SummaryContainer extends Component {
 				{this.props.summaries.map((summary, i) => {
 					const { offset, width, title } = summary
 					return (
-						<div style={{ width: offset * 2 + width }} key={`summary_${i}`}>
+						<React.Fragment>
 							<div
-								style={{
-									marginLeft: offset,
-									width: width,
-									height: 20,
-									border: '2px solid #7494ea',
-									borderTop: 'none',
-								}}
-							/>
-							<a style={{ display: 'block', textAlign: 'center' }}>{title}</a>
-						</div>
+								style={{ width: offset * 2 + width, gridRow: `${i + 1} / span 1`, gridColumn: '1/2' }}
+								key={`summary_${i}`}
+							>
+								<div
+									style={{
+										marginLeft: offset,
+										width: width,
+										height: borderSideHeight,
+										border: '2px solid #7494ea',
+										borderTop: 'none',
+									}}
+								/>
+								<a style={{ display: 'block', textAlign: 'center' }}>{title}</a>
+							</div>
+							{i > 0 ? (
+								<div
+									style={{
+										gridRow: `1/${i + 1}`,
+										gridColumn: `1/2`,
+										marginLeft: offset,
+										width: width,
+										height: '100%',
+										borderRight: '2px dotted #7494ea',
+										borderLeft: '2px dotted #7494ea',
+									}}
+								/>
+							) : null}
+						</React.Fragment>
 					)
 				})}
 			</div>
