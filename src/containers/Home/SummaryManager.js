f891d50d63920409043d1f7e3c675b0842880dfa
Michael Salim <salim_michaelmi@yahoo.com>
2018-03-12 00:30:00 +0000

    feat: made summaries point to correct place

--- src/containers/Home/SummaryManager.js
+++ src/containers/Home/SummaryManager.js
@@ -4,35 +4,71 @@ import gql from 'graphql-tag'
 
 import SummaryContainer from './SummaryContainer'
 
-import { getChapterCountFromBookId } from 'helper'
+import { getChapterCount, getVerseCount } from 'helper'
 
 class SummaryManager extends Component {
-	calculatePosition(bookId, chapter, verse) {
+	constructor(props) {
+		super(props)
+		this.state = {}
+
+		this.selectedFilter = this.selectedFilter.bind(this)
+		this.notSelectedFilter = this.notSelectedFilter.bind(this)
+		this.map = this.map.bind(this)
+		this.calculateSelectedPosition = this.calculateSelectedPosition.bind(this)
+		this.calculateNotSelectedPosition = this.calculateNotSelectedPosition.bind(this)
+	}
+	selectedFilter(selectedBookId) {
+		return ({ startBookId, endBookId }) => selectedBookId === startBookId && startBookId === endBookId
+	}
+	notSelectedFilter() {
+		return ({ startBookId, endBookId }) => startBookId !== endBookId
+	}
+
+	map(calculatePosition) {
+		return verseSummary => {
+			const { title, startBookId, startChapter, startVerse, endBookId, endChapter, endVerse } = verseSummary
+
+			const startPos = calculatePosition(startBookId, startChapter, startVerse)
+			const endPos = calculatePosition(endBookId, endChapter, endVerse)
+
+			const offset = startPos
+			const width = endPos - startPos
+
+			return { title, offset, width }
+		}
+	}
+
+	calculateSelectedPosition(bookId, chapter, verse) {
+		const chapterSize = this.props.getSelectedBookSize(bookId) / getChapterCount(bookId)
 		return (
 			this.props.calculateStartOfBook(bookId) +
-			this.props.getBookSize(bookId) / getChapterCountFromBookId(bookId) * (chapter - 1)
+			chapterSize * (chapter - 1) +
+			chapterSize / getVerseCount(bookId, chapter) * (verse - 1)
 		)
 	}
+	calculateNotSelectedPosition(bookId, chapter, verse) {
+		const chapterSize = this.props.getBookSize(bookId) / getChapterCount(bookId)
+		return this.props.calculateStartOfBook(bookId) + chapterSize * (chapter - 1)
+	}
+
+	formatSummaries(data, filter, map) {
+		return data.filter(filter).map(map)
+	}
+
 	render() {
 		const { loading, error } = this.props.data
+		const { selecting, selectedBookId } = this.props
 
 		if (loading || error) {
 			return <SummaryContainer summaries={[]} />
 		}
 
-		const summaries = this.props.data.versesSummaries.nodes
-			.filter(({ startBookId, endBookId }) => startBookId !== endBookId)
-			.map(verseSummary => {
-				const { title, startBookId, startChapter, startVerse, endBookId, endChapter, endVerse } = verseSummary
+		const summaries = this.formatSummaries(
+			this.props.data.versesSummaries.nodes,
+			selecting ? this.selectedFilter(selectedBookId) : this.notSelectedFilter(),
+			selecting ? this.map(this.calculateSelectedPosition) : this.map(this.calculateNotSelectedPosition),
+		)
 
-				const startPos = this.calculatePosition(startBookId, startChapter, startVerse)
-				const endPos = this.calculatePosition(endBookId, endChapter, endVerse)
-
-				const offset = startPos
-				const width = endPos - startPos
-
-				return { title, offset, width }
-			})
 		return <SummaryContainer summaries={summaries} />
 	}
 }
