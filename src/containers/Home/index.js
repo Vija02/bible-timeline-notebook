7e96c2315a1afe24d16eb7d42e6a9b37b84a2d6d
Michael Salim <salim_michaelmi@yahoo.com>
2018-03-29 17:41:14 +0100

    feat: move book summary to the right

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom'
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 
 import Book from 'containers/Book'
 import Summary from 'containers/Summary'
@@ -10,18 +10,24 @@ import MainTimeline from './MainTimeline'
 import styles from './Home.module.css'
 
 export default class HomeIndex extends Component {
-  render() {
-    return (
-      <div className={styles.container}>
-        <Sidebar className={styles.sidebar} />
-        <MainTimeline className={styles.main} />
-        <div className={styles.bottom}>
-          <Switch>
-            <Route path="/book" component={Book} />
-            <Route path="/summary/:id(\d+)" component={Summary} />
-          </Switch>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div
+				className={
+					this.props.location.pathname.indexOf('/book/') === 0
+						? styles.containerAside
+						: styles.containerNoAside
+				}
+			>
+				<Sidebar className={styles.sidebar} />
+				<MainTimeline className={styles.main} />
+				<div className={styles.aside}>
+					<Route path="/book" component={Book} />
+				</div>
+				<div className={styles.bottom}>
+					<Route path="/summary/:id(\d+)" component={Summary} />
+				</div>
+			</div>
+		)
+	}
 }
