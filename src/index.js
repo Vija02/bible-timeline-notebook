7550e2062054c0f482f618791d80b4bbb4cd1b09
Michael Salim <salim_michaelmi@yahoo.com>
2018-04-22 21:02:46 +0100

    feat: configure sentry

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'react-toastify/dist/ReactToastify.css';
-import Root from 'containers/Root';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Raven from 'raven-js'
+import './index.css'
+import 'react-toastify/dist/ReactToastify.css'
+import Root from 'containers/Root'
+import registerServiceWorker from './registerServiceWorker'
+
+import { version } from '../package.json'
 
 import sizeMe from 'react-sizeme'
-sizeMe.noPlaceholders = true;
+sizeMe.noPlaceholders = true
 
-ReactDOM.render(<Root />, document.getElementById('root'));
-registerServiceWorker();
+if (process.env.REACT_APP_SENTRY_DSN) {
+	Raven.config(process.env.REACT_APP_SENTRY_DSN, {
+		release: version,
+	}).install()
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'))
+registerServiceWorker()
