c6de23f197c757a8deb76d146b7490a9d4b6d229
Michael Salim <salim_michaelmi@yahoo.com>
2018-10-27 12:24:33 +0100

    fix: css messing up due to new css module

--- src/components/DropdownMenu/index.js
+++ src/components/DropdownMenu/index.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 import styles from './DropdownMenu.module.css'
 
 export default class DropdownMenuIndex extends Component {
-  render() {
-    return (
-      <div>
-        <div className={`${styles.dropdown}${this.props.opened ? "-open" : ""}`} onClick={this.props.onClick}>
-          <i className={this.props.iconClassName} />
-          <a className={`${styles.title}${this.props.opened ? "-open" : ""}`}>{this.props.title}</a>
-        </div>
-        <div className={this.props.opened ? styles.body : styles.closedBody}>
-          {
-            this.props.opened ?
-              React.cloneElement(this.props.children)
-              : null
-          }
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<div
+					className={`${styles.dropdown} ${this.props.opened ? styles.dropdownOpen : ''}`}
+					onClick={this.props.onClick}
+				>
+					<i className={this.props.iconClassName} />
+					<a className={`${styles.title} ${this.props.opened ? styles.titleOpen : ''}`}>{this.props.title}</a>
+				</div>
+				<div className={this.props.opened ? styles.body : styles.closedBody}>
+					{this.props.opened ? React.cloneElement(this.props.children) : null}
+				</div>
+			</div>
+		)
+	}
 }
 
-DropdownMenuIndex.defaultProps = { iconClassName: "", title: "", opened: false, onClick: () => { } }
+DropdownMenuIndex.defaultProps = { iconClassName: '', title: '', opened: false, onClick: () => {} }
