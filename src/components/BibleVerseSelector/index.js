46addba446bddf1515ec87889e17a705050e6650
Michael Salim <salim_michaelmi@yahoo.com>
2018-10-29 01:17:40 +0000

    fix: input css not within container

--- src/components/BibleVerseSelector/index.js
+++ src/components/BibleVerseSelector/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Autocomplete from 'react-autocomplete'
 import deepEqual from 'deep-equal'
 
@@ -7,114 +7,130 @@ import { oldTestament } from 'helper'
 import styles from './BibleVerseSelector.module.css'
 
 export default class BibleVerseSelector extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      book: "", chapter: "", verse: "", // Input state
-      chapterCount: null, verseCount: null, // To show placeholder limit 
-      chapterDisabled: true, verseDisabled: true,
-      lastValue: undefined, // Used to send onChange only when different value
-    };
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			book: '',
+			chapter: '',
+			verse: '', // Input state
+			chapterCount: null,
+			verseCount: null, // To show placeholder limit
+			chapterDisabled: true,
+			verseDisabled: true,
+			lastValue: undefined, // Used to send onChange only when different value
+		}
+	}
 
-  validate(stateName, value) {
-    const placeholderCount = { chapterCount: null, verseCount: null }
-    const nextState = { ...this.state, ...placeholderCount, chapterDisabled: true, verseDisabled: true }
-    // Update input
-    nextState[stateName] = value
+	validate(stateName, value) {
+		const placeholderCount = { chapterCount: null, verseCount: null }
+		const nextState = { ...this.state, ...placeholderCount, chapterDisabled: true, verseDisabled: true }
+		// Update input
+		nextState[stateName] = value
 
-    const book = oldTestament.find(el => el.bookName.toLowerCase() === nextState.book.toLowerCase())
-    if (book) {
-      nextState.chapterDisabled = false;
+		const book = oldTestament.find(el => el.bookName.toLowerCase() === nextState.book.toLowerCase())
+		if (book) {
+			nextState.chapterDisabled = false
 
-      const chapter = parseInt(nextState.chapter, 10)
-      const chapterCount = book.chaptersCount
-      const chapterValid = chapter > 0 && chapter < chapterCount + 1
+			const chapter = parseInt(nextState.chapter, 10)
+			const chapterCount = book.chaptersCount
+			const chapterValid = chapter > 0 && chapter < chapterCount + 1
 
-      nextState.chapterCount = chapterCount;
+			nextState.chapterCount = chapterCount
 
-      if (chapterValid) {
-        nextState.verseDisabled = false;
+			if (chapterValid) {
+				nextState.verseDisabled = false
 
-        const verse = parseInt(nextState.verse, 10)
-        const verseCount = book.chapters.find(chap => parseInt(chap.chapter, 10) === parseInt(nextState.chapter, 10)).verseCount
-        const verseValid = verse > 0 && verse < verseCount + 1
+				const verse = parseInt(nextState.verse, 10)
+				const verseCount = book.chapters.find(
+					chap => parseInt(chap.chapter, 10) === parseInt(nextState.chapter, 10),
+				).verseCount
+				const verseValid = verse > 0 && verse < verseCount + 1
 
-        nextState.verseCount = verseCount;
+				nextState.verseCount = verseCount
 
-        if (verseValid) {
-          this.setState(nextState)
-          this.onComplete(book.bookId, chapter, verse)
-          return;
-        }
-      }
-    }
-    this.setState(nextState)
-    this.onIncomplete()
-  }
+				if (verseValid) {
+					this.setState(nextState)
+					this.onComplete(book.bookId, chapter, verse)
+					return
+				}
+			}
+		}
+		this.setState(nextState)
+		this.onIncomplete()
+	}
 
-  onChange(val) {
-    if (!deepEqual(val, this.state.lastValue)) {
-      this.props.onChange(val)
-      this.setState({ lastValue: val })
-    }
-  }
+	onChange(val) {
+		if (!deepEqual(val, this.state.lastValue)) {
+			this.props.onChange(val)
+			this.setState({ lastValue: val })
+		}
+	}
 
-  onComplete(book, chapter, verse) {
-    this.onChange({ book, chapter, verse })
-  }
-  onIncomplete() {
-    this.onChange(null)
-  }
+	onComplete(book, chapter, verse) {
+		this.onChange({ book, chapter, verse })
+	}
+	onIncomplete() {
+		this.onChange(null)
+	}
 
-  renderItem(item, highlighted) {
-    return (
-      <div
-        key={item.bookId}
-        style={{ backgroundColor: highlighted ? '#868686' : 'transparent' }}
-      >
-        {item.bookName}
-      </div>
-    )
-  }
-  render() {
-    return (
-      <div>
-        <Autocomplete
-          data-testid="book"
-          items={oldTestament}
-          getItemValue={(item) => item.bookName}
-          shouldItemRender={(item, value) => item.bookName.toLowerCase().indexOf(value.toLowerCase()) > -1}
-          renderItem={this.renderItem}
-          renderMenu={(items, value, style) => {console.log(items);return <div style={style} className={styles.menu} children={items} />}}
-          renderInput={(props) => <input type="text" placeholder="Book" {...props} />}
-          value={this.state.book}
-          onChange={(e) => { this.validate("book", e.target.value) }}
-          onSelect={(val) => { this.validate("book", val) }}
-        />
-        {' '}
-        <input
-          data-testid="chapter"
-          className={styles.numberInput}
-          type="text"
-          placeholder={this.state.chapterDisabled ? "Chapter" : `1-${this.state.chapterCount}`}
-          disabled={this.state.chapterDisabled}
-          value={this.state.chapter}
-          onChange={(e) => { this.validate("chapter", e.target.value) }}
-        />
-        :
-        <input
-          data-testid="verse"
-          className={styles.numberInput}
-          type="text"
-          placeholder={this.state.verseDisabled ? "Verse" : `1-${this.state.verseCount}`}
-          disabled={this.state.verseDisabled}
-          value={this.state.verse}
-          onChange={(e) => { this.validate("verse", e.target.value) }}
-        />
-      </div>
-    );
-  }
+	renderItem(item, highlighted) {
+		return (
+			<div key={item.bookId} style={{ backgroundColor: highlighted ? '#868686' : 'transparent' }}>
+				{item.bookName}
+			</div>
+		)
+	}
+	render() {
+		return (
+			<div className={styles.container}>
+				<Autocomplete
+					data-testid="book"
+					items={oldTestament}
+					getItemValue={item => item.bookName}
+					shouldItemRender={(item, value) => item.bookName.toLowerCase().indexOf(value.toLowerCase()) > -1}
+					renderItem={this.renderItem}
+					renderMenu={(items, value, style) => {
+						console.log(items)
+						return <div style={style} className={styles.menu} children={items} />
+					}}
+					renderInput={props => <input type="text" placeholder="Book" {...props} />}
+					value={this.state.book}
+					onChange={e => {
+						this.validate('book', e.target.value)
+					}}
+					onSelect={val => {
+						this.validate('book', val)
+					}}
+					wrapperStyle={{ display: 'contents' }}
+				/>{' '}
+				<div className={styles.chapterVerseContainer}>
+					<input
+						data-testid="chapter"
+						className={styles.numberInput}
+						type="text"
+						placeholder={this.state.chapterDisabled ? 'Chapter' : `1-${this.state.chapterCount}`}
+						disabled={this.state.chapterDisabled}
+						value={this.state.chapter}
+						onChange={e => {
+							this.validate('chapter', e.target.value)
+						}}
+					/>
+					:
+					<input
+						data-testid="verse"
+						className={styles.numberInput}
+						type="text"
+						placeholder={this.state.verseDisabled ? 'Verse' : `1-${this.state.verseCount}`}
+						disabled={this.state.verseDisabled}
+						value={this.state.verse}
+						onChange={e => {
+							this.validate('verse', e.target.value)
+						}}
+					/>
+				</div>
+			</div>
+		)
+	}
 }
 
-BibleVerseSelector.defaultProps = { onChange: () => { } };
+BibleVerseSelector.defaultProps = { onChange: () => {} }
