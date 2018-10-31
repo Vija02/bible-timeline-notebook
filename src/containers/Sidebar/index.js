58270e28f0b33e1a73b6802c4feb7a574d74d7c7
Michael Salim <salim_michaelmi@yahoo.com>
2018-10-31 23:35:06 +0000

    feat: add link to github

--- src/containers/Sidebar/index.js
+++ src/containers/Sidebar/index.js
@@ -14,20 +14,30 @@ export default class SideBarIndex extends Component {
 	render() {
 		return (
 			<div className={`${this.props.className} ${styles.container}`}>
-				<DropdownMenu
-					iconClassName="fas fa-plus"
-					title="Add Summary"
-					onClick={() => {
-						this.setState({ opened: !this.state.opened })
-					}}
-					opened={this.state.opened}
-				>
-					<AddSummary />
-				</DropdownMenu>
-				<div style={{ paddingLeft: 15, paddingRight: 15 }}>
-					<h2>Notice</h2>
-					<p>The project is currently on progress. Certain features might or might not work.</p>
+				<div className={styles.topContainer}>
+					<DropdownMenu
+						iconClassName="fas fa-plus"
+						title="Add Summary"
+						onClick={() => {
+							this.setState({ opened: !this.state.opened })
+						}}
+						opened={this.state.opened}
+					>
+						<AddSummary />
+					</DropdownMenu>
+					<div style={{ paddingLeft: 15, paddingRight: 15 }}>
+						<h2>Notice</h2>
+						<p>The project is currently on progress. Certain features might or might not work.</p>
+					</div>
 				</div>
+				<a
+					href="https://github.com/Vija02/bible-timeline-notebook"
+					target="_blank"
+					rel="noopener noreferrer"
+					className={styles.bottomContainer}
+				>
+					View on Github <i class="fab fa-github" />
+				</a>
 			</div>
 		)
 	}
