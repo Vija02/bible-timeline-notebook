00ec8106e4e64b263c6aa8ac32fa17c86f6046b4
Michael Salim <salim_michaelmi@yahoo.com>
2019-04-10 00:24:19 +0100

    feat: update some wording and increase readability

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -13,13 +13,13 @@ export default class AboutIndex extends Component {
 					<div className="cardContainer">
 						<h2 className="cardTitle">Resources</h2>
 						<p className={styles.text}>
-							If you haven't, it is strongly recommended that you finish reading the bible at least once.
-							This allows you to understand the bigger picture of the bible. There are <i>a lot</i> of
-							excellent reading plans readily available on the internet. Pick any of them and stick to it.
-							It gets easier over time!
+							If you haven't already done so, it is strongly recommended that you finish reading the bible
+							at least once. This allows you to understand the bigger picture of the bible. There are{' '}
+							<i>a lot</i> of excellent reading plans readily available on the internet. Pick any of them
+							and stick to it. It gets easier over time!
 							<br />
 							<br />
-							Here are some to get you started:
+							Here are some links to get you started:
 						</p>
 
 						<ResourcesRow
@@ -39,7 +39,7 @@ export default class AboutIndex extends Component {
 							content="A 1-year reading plan accompanied by great animated video explanations."
 							link="https://thebibleproject.com/other-resources/other/"
 						/>
-
+						<hr />
 						<p className={styles.muchMoreText}>
 							And much more!{' '}
 							<Link className={styles.link} to="/contact-me">
@@ -51,8 +51,10 @@ export default class AboutIndex extends Component {
 					<div className="cardContainer">
 						<h2 className="cardTitle">Using this website</h2>
 						<p className={styles.text}>
-							If you are here, you probably want to know the bible better. First, read the whole bible!
-							Look at the resources above if you haven't. <br />
+							If you are here, you probably want to know the bible better.
+							<br />
+							<br />
+							First, <b>read the whole bible!</b> Look at the resources above if you haven't. <br />
 							Once you're done with that, there are always other reading plans to follow. If one catches
 							your attention, great! If not, that's fine too.
 							<br />
