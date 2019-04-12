b65e9ea2ed1b83cab0182ffe7e98bd099556d5c5
Michael Salim <salim_michaelmi@yahoo.com>
2019-04-12 23:37:41 +0100

    refactor: move summary form stuff into its own file

---
+++ src/containers/ChapterDetail/OwnSummary.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import { toast } from 'react-toastify'
+
+import LoginModalToggler from 'containers/Navbar/LoginModalToggler'
+import ChapterSummaryFormIndex from 'containers/Forms/ChapterSummaryForm'
+
+import Mutation from 'components/Mutation'
+import { useAuth } from 'providers/Auth'
+
+import { CREATE_CHAPTER_SUMMARY, ALL_CHAPTER_SUMMARIES } from './queries'
+
+export default ({ bookId, chapter }) => {
+	const { jwt, userId } = useAuth()
+	return (
+		<>
+			{!!jwt ? (
+				<Mutation mutation={CREATE_CHAPTER_SUMMARY}>
+					{createChapterSummary => (
+						<ChapterSummaryFormIndex
+							onSubmit={data => {
+								return addSummary(createChapterSummary, userId, bookId, chapter, data)
+							}}
+						/>
+					)}
+				</Mutation>
+			) : (
+				<NotLoggedIn />
+			)}
+		</>
+	)
+}
+
+const addSummary = (createChapterSummary, userId, bookId, chapter, data) => {
+	const { summary } = data
+
+	return createChapterSummary({
+		variables: {
+			userId,
+			summary,
+			bookId,
+			chapter,
+		},
+		update: (proxy, res) => {
+			const chapterSummary = res.data.createChapterSummary.chapterSummary
+
+			let updatedData = proxy.readQuery({ query: ALL_CHAPTER_SUMMARIES, variables: { bookId, chapter } })
+
+			updatedData = {
+				...updatedData,
+				allChapterSummaries: {
+					...updatedData.allChapterSummaries,
+					nodes: [
+						// Avoid duplication
+						chapterSummary,
+						...updatedData.allChapterSummaries.nodes.filter(val => val.nodeId !== chapterSummary.nodeId),
+					],
+				},
+			}
+
+			proxy.writeQuery({
+				query: ALL_CHAPTER_SUMMARIES,
+				variables: { bookId, chapter },
+				data: updatedData,
+			})
+		},
+	})
+		.then(() => {
+			toast.success(
+				<p>
+					<i className="far fa-check-circle" /> Successfully posted!
+				</p>,
+			)
+		})
+		.catch(err => {
+			console.error(err)
+			toast.error('Error creating the post. Please try again later.')
+			throw new Error()
+		})
+}
+
+const NotLoggedIn = () => {
+	return (
+		<div>
+			<LoginModalToggler>
+				<span className="link">Register or Login now to post!</span>
+			</LoginModalToggler>
+		</div>
+	)
+}
--- src/containers/ChapterDetail/index.js
+++ src/containers/ChapterDetail/index.js
@@ -1,23 +1,14 @@
 import React from 'react'
 import { Redirect } from 'react-router-dom'
-import { toast } from 'react-toastify'
 
-import LoginModalToggler from 'containers/Navbar/LoginModalToggler'
-import ChapterSummaryFormIndex from 'containers/Forms/ChapterSummaryForm'
-
+import OwnSummary from './OwnSummary'
 import AllChapterSummaries from './AllChapterSummaries'
-
-import Mutation from 'components/Mutation'
-import { useAuth } from 'providers/Auth'
 
 import { getChapter, bookIdFromName, unformatBook, getChapterCount } from 'helper'
 
-import { CREATE_CHAPTER_SUMMARY, ALL_CHAPTER_SUMMARIES } from './queries'
 import styles from './index.module.css'
 
 export default props => {
-	const { userId, jwt } = useAuth()
-
 	const bookId = bookIdFromName(props.match.params.bookName)
 	const chapter = parseInt(props.match.params.chapter, 10)
 
@@ -57,80 +48,10 @@ export default props => {
 			<div className="padContainer">
 				<h4 className={styles.thoughtsTitle}>Thoughts</h4>
 
-				{!!jwt ? (
-					<Mutation mutation={CREATE_CHAPTER_SUMMARY}>
-						{createChapterSummary => (
-							<ChapterSummaryFormIndex
-								onSubmit={data => {
-									return addSummary(createChapterSummary, userId, bookId, chapter, data)
-								}}
-							/>
-						)}
-					</Mutation>
-				) : (
-					<NotLoggedIn />
-				)}
+				<OwnSummary />
 
 				<AllChapterSummaries bookId={bookId} chapter={chapter} />
 			</div>
-		</div>
-	)
-}
-
-const addSummary = (createChapterSummary, userId, bookId, chapter, data) => {
-	const { summary } = data
-
-	return createChapterSummary({
-		variables: {
-			userId,
-			summary,
-			bookId,
-			chapter,
-		},
-		update: (proxy, res) => {
-			const chapterSummary = res.data.createChapterSummary.chapterSummary
-
-			let updatedData = proxy.readQuery({ query: ALL_CHAPTER_SUMMARIES, variables: { bookId, chapter } })
-
-			updatedData = {
-				...updatedData,
-				allChapterSummaries: {
-					...updatedData.allChapterSummaries,
-					nodes: [
-						// Avoid duplication
-						chapterSummary,
-						...updatedData.allChapterSummaries.nodes.filter(val => val.nodeId !== chapterSummary.nodeId),
-					],
-				},
-			}
-
-			proxy.writeQuery({
-				query: ALL_CHAPTER_SUMMARIES,
-				variables: { bookId, chapter },
-				data: updatedData,
-			})
-		},
-	})
-		.then(() => {
-			toast.success(
-				<p>
-					<i className="far fa-check-circle" /> Successfully posted!
-				</p>,
-			)
-		})
-		.catch(err => {
-			console.error(err)
-			toast.error('Error creating the post. Please try again later.')
-			throw new Error()
-		})
-}
-
-const NotLoggedIn = () => {
-	return (
-		<div>
-			<LoginModalToggler>
-				<span className="link">Register or Login now to post!</span>
-			</LoginModalToggler>
 		</div>
 	)
 }
