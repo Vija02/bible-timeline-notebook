7d24d42ceb775bc2dccb436c08e1a71cb6101e93
Michael Salim <salim_michaelmi@yahoo.com>
2020-01-19 11:28:28 +0000

    deps: update and simplify apollo implementation

--- src/getApolloClient.js
+++ src/getApolloClient.js
@@ -1,34 +1,24 @@
-import ApolloClient from 'apollo-client'
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { setContext } from 'apollo-link-context'
-import { onError } from 'apollo-link-error'
+import ApolloClient from 'apollo-boost'
 
 export default function(logoutFunction = () => {}, jwt) {
-	const cache = new InMemoryCache({
-		dataIdFromObject: o => o.nodeId,
-	})
-	const middlewareLink = setContext(() => {
-		if (jwt) {
-			return {
-				headers: {
-					Authorization: `Bearer ${jwt}`,
-				},
-			}
-		}
-		return {}
-	})
-	const logoutLink = onError(({ networkError }) => {
-		if (networkError && networkError.statusCode === 401) {
-			logoutFunction()
-		}
-	})
-	const httpLink = createHttpLink({
-		uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
-	})
 	return new ApolloClient({
-		cache: cache,
-		link: logoutLink.concat(middlewareLink.concat(httpLink)),
-		defaultOptions: { query: { notifyOnNetworkStatusChange: true } },
+		uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+		fetchOptions: {
+			credentials: 'include',
+		},
+		request: async operation => {
+			if (jwt) {
+				operation.setContext({
+					headers: {
+						authorization: `Bearer ${jwt}`,
+					},
+				})
+			}
+		},
+		onError: ({ networkError }) => {
+			if (networkError && networkError.statusCode === 401) {
+				logoutFunction()
+			}
+		},
 	})
 }
